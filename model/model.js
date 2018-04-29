3a33ce4c5649d134eef4b9d4977d6d11ec810824
purpose50 <purposenigeria@gmail.com>
2018-04-29 16:29:46 +0100

    setup-mongoose

---
+++ model/model.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const todoSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
+
+const Todo = mongoose.model('Todo', todoSchema);
+export default Todo;
+
